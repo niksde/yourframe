69ffee481449310c2de130d9dab03e01d50c240a
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2020-10-21 12:02:35 +0530

    styles fixed

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -4,16 +4,18 @@ import ImageCard from "./ImageCard";
 export default function ImageGrid({ items = [] }) {
   return (
     <div className="grid-container">
-      {items.length !== 0
-        ? items.map((image, index) => (
-            <ImageCard
-              imageName={image.imageName}
-              imageUrl={image.imageUrl}
-              className="grid-item"
-              key={image.imageName + "-" + index}
-            />
-          ))
-        : "loading..."}
+      {items.length !== 0 ? (
+        items.map((image, index) => (
+          <ImageCard
+            imageName={image.imageName}
+            imageUrl={image.imageUrl}
+            className="grid-item"
+            key={image.imageName + "-" + index}
+          />
+        ))
+      ) : (
+        <h2>loading...</h2>
+      )}
     </div>
   );
 }
