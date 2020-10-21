c43a2761013cf5b86baa377062a0d8c31b1302e7
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2020-10-21 10:07:59 +0530

    added readme

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL + "/api";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    // toast.error("An unexpected error occurrred.");
+    console.error(error.response.status, " : ", error);
+  }
+
+  return Promise.reject(error);
+});
 
 export default {
   get: axios.get,
